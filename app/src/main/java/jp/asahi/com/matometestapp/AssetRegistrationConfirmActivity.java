7c15909c0f0f20fe39ca9b23e712ca4039cb0e8d
nakamura <nakamura@local>
2016-11-17 23:26:21 +0900

    Write asset_registration_confirm_activity

---
+++ app/src/main/java/jp/asahi/com/matometestapp/AssetRegistrationConfirmActivity.java
@@ -0,0 +1,14 @@
+package jp.asahi.com.matometestapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AssetRegistrationConfirmActivity extends TabFooterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.asset_registration_confirm_activity);
+    }
+}
